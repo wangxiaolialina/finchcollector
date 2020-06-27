3c8709436c7eb30c2d9dfb343bbeec8287eb8338
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-06-27 14:49:19 -0400

    add url and view function to view all finches in template

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('finches/', views.finches_index, name='index')
 ]
 
