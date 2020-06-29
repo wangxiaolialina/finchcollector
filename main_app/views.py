4904151785c14c961419b3b89d799e7dad8ac876
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-06-29 18:05:24 -0400

    add home page

--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
-
 # Add the following import
 from django.http import HttpResponse
+from .models import Finch
 
 # Define the home view
 
 
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
 
 
 def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches': finches})
+
+def finches_detail(request,finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request,'finches/detail.html', {'finch':finch})
