d776be29dab7c4ddcdad1682ba327be1bc7159fb
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-07-07 10:33:26 -0400

    feeding feature

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 # Create your models here.
 class Finch(models.Model):
   name = models.CharField(max_length = 100)
@@ -13,3 +18,20 @@ class Finch(models.Model):
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={'finch_id': self.id})
+
+class Feeding(models.Model):
+  date = models.DateField('feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+  cat = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    return f"{self.get_meal_display()} on {self.date}"
+
+  # change the default sort
+  class Meta:
+    ordering = ['-date']
