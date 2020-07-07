35ef5d0d87b213d7c6187597353d851d58e830e2
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-07-07 10:33:43 -0400

    register feeding on admin site

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Finch
+from .models import Finch, Feeding
 # Register your models here.
 admin.site.register(Finch)
+admin.site.register(Feeding)
