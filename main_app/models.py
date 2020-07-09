dd4dd1579afbd1213e1c1a168ceca9d2ad19901f
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-07-09 16:07:12 -0400

    add toy model

--- main_app/models.py
+++ main_app/models.py
@@ -7,11 +7,23 @@ MEALS = (
     ('D', 'Dinner')
 )
 # Create your models here.
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+    
 class Finch(models.Model):
   name = models.CharField(max_length = 100)
   breed = models.CharField(max_length = 100)
   description = models.TextField(max_length = 100)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
 
   def __str__(self):
     return self.name
