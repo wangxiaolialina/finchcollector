03447f8cbccb62636909eafe8b75c3bdc08f6643
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-07-09 18:54:04 -0400

    add urls for toydetail, toyupdate and toydelete

--- main_app/urls.py
+++ main_app/urls.py
@@ -13,7 +13,10 @@ urlpatterns = [
 
     path('finches/<int:finch_id>/assoc_toy/<int:toy_id>/', views.assoc_toy, name='assoc_toy'),
     path('toys/', views.ToyList.as_view(), name='toys_index'),
+      path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toys_detail'),
     path('toys/create/', views.ToyCreate.as_view(), name='toys_create'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toys_update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toys_delete'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup', views.signup, name='signup'),
 
