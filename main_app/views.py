6d73fd4ecc2123f10bdd037375c3ff0d2749dab1
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-07-09 23:04:49 -0400

    add login requirement

--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,6 @@ from django.shortcuts import render,redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 # Add the following import
-from django.views.generic.edit import CreateView
-from django.views.generic.edit import UpdateView
-from django.views.generic.edit import DeleteView
 from .models import Finch,Toy
 from .forms import FeedingForm
 from django.contrib.auth import login
@@ -39,14 +36,17 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def finches_index(request):
     finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches': finches})
 
+@login_required
 def finches_detail(request,finch_id):
     finch = Finch.objects.get(id=finch_id)
+    toys_finch_doesnt_have = Toy.objects.exclude(id__in = finch.toys.all().values_list('id'))
     feeding_form = FeedingForm()
-    return render(request,'finches/detail.html', {'finch':finch,'feeding_form': feeding_form})
+    return render(request,'finches/detail.html', {'finch':finch,'feeding_form': feeding_form, 'toys': toys_finch_doesnt_have})
 
 class FinchCreate(CreateView):
     model = Finch
@@ -60,6 +60,7 @@ class FinchDelete(DeleteView):
     model = Finch
     success_url = '/finches/'
 
+@login_required
 def add_feeding(request, finch_id):
   # create a ModelForm instance using the data in request.POST
   form = FeedingForm(request.POST)
@@ -71,10 +72,16 @@ def add_feeding(request, finch_id):
     new_feeding.save()
   return redirect('detail', finch_id=finch_id)
 
+@login_required
 def assoc_toy(request, finch_id, toy_id):
-  # Note that you can pass a toy's id instead of the whole object
-  Cat.objects.get(id=finch_id).toys.add(toy_id)
-  return redirect('detail', finch_id=cat_id)
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+@login_required
+def unassoc_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.remove(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
 
 class ToyList(LoginRequiredMixin, ListView):
   model = Toy
